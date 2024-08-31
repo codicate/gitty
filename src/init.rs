31b7effe44c1a94dae2a8b939d4ef8a4e4e0f913
Henry Liu <codicate@gmail.com>
2024-08-31 00:36:39 -0400

    init does not initialize twice

--- src/init.rs
+++ src/init.rs
@@ -1,4 +1,5 @@
-use std::io::Result;
+use std::io::{Error, ErrorKind, Result};
+use std::path::Path;
 use std::{env, fs};
 
 pub fn main() {
@@ -9,9 +10,15 @@ pub fn main() {
 }
 
 fn init() -> Result<()> {
-    let dir_path = ".gyat";
+    let path = Path::new(".gyat");
+    if path.is_dir() {
+        return Err(Error::new(
+            ErrorKind::Other,
+            "This repository has already been initialized",
+        ));
+    }
 
-    fs::create_dir(dir_path)?;
+    fs::create_dir(path)?;
     fs::create_dir(".gyat/objects")?;
 
     let cwd = env::current_dir()?;
--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,6 @@ mod reset;
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
-    /// Turn debugging information on
-    #[arg(short, long, action = clap::ArgAction::Count)]
-    debug: u8,
-
     #[command(subcommand)]
     command: Option<Commands>,
 }
@@ -20,6 +16,7 @@ struct Cli {
 enum Commands {
     /// Create an empty Git repository or reinitialize an existing one
     Init {},
+    // DEBUG
     Reset {},
 
     /// Compute object ID and optionally create an object from a file
@@ -30,6 +27,7 @@ enum Commands {
         write: bool,
     },
 
+    /// Provide contents or details of repository objects
     CatFile {
         hash: String,
     },
@@ -37,15 +35,6 @@ enum Commands {
 
 fn main() {
     let cli = Cli::parse();
-
-    // You can see how many times a particular flag or argument occurred
-    // Note, only flags can have multiple occurrences
-    match cli.debug {
-        0 => println!("Debug mode is off"),
-        1 => println!("Debug mode is kind of on"),
-        2 => println!("Debug mode is on"),
-        _ => println!("Don't be crazy"),
-    }
 
     // You can check for the existence of subcommands, and if found use their
     // matches just as you would the top level cmd
